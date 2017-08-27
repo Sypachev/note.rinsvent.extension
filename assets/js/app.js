f6146d388ed1ed99a876fa9b9e862e534fda8caf
rinsvent <2Wdtnjr>
2017-08-27 20:24:26 +0700

    add interface

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,7 +33,7 @@ class App {
         return '<div class="note"><div class="date">' + note["date_created"]
             + '</div><div class="name">' + note["name"]
             + '</div><div class="text">' + note["text"]
-            + '</div></div>';
+            + '</div><hr /></div>';
     }
 
     renderNotes(resp, xhr) {
@@ -49,17 +49,30 @@ class App {
         let notesId = app.notesId;
         let nodeNotes = $("#" + notesId);
         if(nodeNotes.length == 0){
-           $(".container").append('<div id="' + notesId + '">');
+           $(".container .tab.search").append('<div id="' + notesId + '">');
             nodeNotes = $("#" + notesId);
         }
         nodeNotes.html("");
+
+        if(resp.items.length > 0){
+            nodeNotes.append('<div class="title">Найдено:</div>');
+        }else{
+            nodeNotes.append('<div class="title">Ничего не найдено</div>');
+        }
 
         for(let note in resp.items){
             app.renderNote(resp.items[note]);
         }
     }
 
-    getNotes() {
+    getNotes(node) {
+
+        let formData = false;
+        if(typeof node == "object"){
+            let form = this.getForm(node);
+            formData = this.getFormData(form);
+        }
+
         var that = this;
         this.method = "get-notes";
         let url = this.domain + this.method;
@@ -76,8 +89,20 @@ class App {
             }
         };
 
+        for(let key in formData){
+            params.parameters[formData[key].name] = formData[key].value;
+        }
+
         this.send(url, this.renderNotes, params);
     };
+
+    getForm(node){
+        return $(node).closest("form");
+    }
+
+    getFormData(node){
+        return $(node).serializeArray()
+    }
 
     send(url, callback, params) {
         var that = this;
@@ -97,5 +122,40 @@ class App {
 
     updateNote() {
 
+    }
+
+
+    renderAddNote(){
+
+    }
+
+    addNote(node){
+        let formData = false;
+        if(typeof node == "object"){
+            let form = this.getForm(node);
+            formData = this.getFormData(form);
+        }
+
+        var that = this;
+        this.method = "add-note";
+        let url = this.domain + this.method;
+
+        let params = {
+            'method': 'GET',
+            'headers': {
+                'GData-Version': '3.0',
+                'If-Match': '*'
+            },
+            'parameters': {
+                'alt': 'json',
+                'max-results': 100,
+            }
+        };
+
+        for(let key in formData){
+            params.parameters[formData[key].name] = formData[key].value;
+        }
+
+        this.send(url, this.renderAddNote, params);
     }
 }
--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,5 +1,33 @@
 'use strict';
 
 var app = new App();
-app.getNotes();
+//auth
+if(!app.bgPage.oauth.hasToken()){
+    app.bgPage.oauth.authorize(function(){});
+};
+
+$(function(){
+    $(".search form button").on("click", function(){
+        app.getNotes(this);
+    });
+    $(".add form button").on("click", function(){
+        app.addNote(this);
+    });
+    $(".search form input.query").on('keydown', function(e){
+        if (e.keyCode == 13) {
+            e.preventDefault();
+            let button = $(".tab.active form button");
+            app.getNotes(button);
+            return false;
+        }
+    });
+
+    $(".header_item").on("click", function(){
+        $(".header_item, .tab").removeClass("active");
+        $(this).addClass("active");
+        $('.tab.' + $(this).data("tab")).addClass("active");
+
+    });
+});
+
 
